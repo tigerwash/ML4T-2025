1ae18edf4d1d5eec79f5f80c9c2d53a9ac912696
tigerwash <liaolongtai@gmail.com>
2025-04-02 21:48:51 -0700

    p7

--- qlearning_robot/QLearner.py
+++ qlearning_robot/QLearner.py
@@ -20,89 +20,153 @@ as potential employers. However, sharing with other current or future
 students of CS 7646 is prohibited and subject to being investigated as a  		  	   		 	 	 			  		 			     			  	 
 GT honor code violation.  		  	   		 	 	 			  		 			     			  	 
   		  	   		 	 	 			  		 			     			  	 
------do not edit anything above this line---  		  	   		 	 	 			  		 			     			  	 
-  		  	   		 	 	 			  		 			     			  	 
-Student Name: Tucker Balch (replace with your name)  		  	   		 	 	 			  		 			     			  	 
-GT User ID: tb34 (replace with your User ID)  		  	   		 	 	 			  		 			     			  	 
-GT ID: 900897987 (replace with your GT ID)  		  	   		 	 	 			  		 			     			  	 
+-----do not edit anything above this line---  	
+	  	   		 	 	 			  		 			     			  	 
+Student Name: Longtai Liao		  	   		  	  			  		 			 	 	 		 		 	 		 		 	 		  	 	 			  	 
+GT User ID: lliao32    	   		  	  			  		 			 	 	 		 		 	 		 		 	 		  	 	 			  	 
+GT ID: 903648350  		  	   		 	 	 			  		 			     			  	 
 """  		  	   		 	 	 			  		 			     			  	 
   		  	   		 	 	 			  		 			     			  	 
 import random as rand  		  	   		 	 	 			  		 			     			  	 
   		  	   		 	 	 			  		 			     			  	 
-import numpy as np  		  	   		 	 	 			  		 			     			  	 
+import numpy as np
+
   		  	   		 	 	 			  		 			     			  	 
-  		  	   		 	 	 			  		 			     			  	 
-class QLearner(object):  		  	   		 	 	 			  		 			     			  	 
-    """  		  	   		 	 	 			  		 			     			  	 
-    This is a Q learner object.  		  	   		 	 	 			  		 			     			  	 
-  		  	   		 	 	 			  		 			     			  	 
-    :param num_states: The number of states to consider.  		  	   		 	 	 			  		 			     			  	 
-    :type num_states: int  		  	   		 	 	 			  		 			     			  	 
-    :param num_actions: The number of actions available..  		  	   		 	 	 			  		 			     			  	 
-    :type num_actions: int  		  	   		 	 	 			  		 			     			  	 
-    :param alpha: The learning rate used in the update rule. Should range between 0.0 and 1.0 with 0.2 as a typical value.  		  	   		 	 	 			  		 			     			  	 
-    :type alpha: float  		  	   		 	 	 			  		 			     			  	 
-    :param gamma: The discount rate used in the update rule. Should range between 0.0 and 1.0 with 0.9 as a typical value.  		  	   		 	 	 			  		 			     			  	 
-    :type gamma: float  		  	   		 	 	 			  		 			     			  	 
-    :param rar: Random action rate: the probability of selecting a random action at each step. Should range between 0.0 (no random actions) to 1.0 (always random action) with 0.5 as a typical value.  		  	   		 	 	 			  		 			     			  	 
-    :type rar: float  		  	   		 	 	 			  		 			     			  	 
-    :param radr: Random action decay rate, after each update, rar = rar * radr. Ranges between 0.0 (immediate decay to 0) and 1.0 (no decay). Typically 0.99.  		  	   		 	 	 			  		 			     			  	 
-    :type radr: float  		  	   		 	 	 			  		 			     			  	 
-    :param dyna: The number of dyna updates for each regular update. When Dyna is used, 200 is a typical value.  		  	   		 	 	 			  		 			     			  	 
-    :type dyna: int  		  	   		 	 	 			  		 			     			  	 
-    :param verbose: If “verbose” is True, your code can print out information for debugging.  		  	   		 	 	 			  		 			     			  	 
-    :type verbose: bool  		  	   		 	 	 			  		 			     			  	 
-    """  		  	   		 	 	 			  		 			     			  	 
-    def __init__(  		  	   		 	 	 			  		 			     			  	 
-        self,  		  	   		 	 	 			  		 			     			  	 
-        num_states=100,  		  	   		 	 	 			  		 			     			  	 
-        num_actions=4,  		  	   		 	 	 			  		 			     			  	 
-        alpha=0.2,  		  	   		 	 	 			  		 			     			  	 
-        gamma=0.9,  		  	   		 	 	 			  		 			     			  	 
-        rar=0.5,  		  	   		 	 	 			  		 			     			  	 
-        radr=0.99,  		  	   		 	 	 			  		 			     			  	 
-        dyna=0,  		  	   		 	 	 			  		 			     			  	 
-        verbose=False,  		  	   		 	 	 			  		 			     			  	 
-    ):  		  	   		 	 	 			  		 			     			  	 
-        """  		  	   		 	 	 			  		 			     			  	 
-        Constructor method  		  	   		 	 	 			  		 			     			  	 
-        """  		  	   		 	 	 			  		 			     			  	 
-        self.verbose = verbose  		  	   		 	 	 			  		 			     			  	 
-        self.num_actions = num_actions  		  	   		 	 	 			  		 			     			  	 
-        self.s = 0  		  	   		 	 	 			  		 			     			  	 
-        self.a = 0  		  	   		 	 	 			  		 			     			  	 
-  		  	   		 	 	 			  		 			     			  	 
-    def querysetstate(self, s):  		  	   		 	 	 			  		 			     			  	 
-        """  		  	   		 	 	 			  		 			     			  	 
-        Update the state without updating the Q-table  		  	   		 	 	 			  		 			     			  	 
-  		  	   		 	 	 			  		 			     			  	 
-        :param s: The new state  		  	   		 	 	 			  		 			     			  	 
-        :type s: int  		  	   		 	 	 			  		 			     			  	 
-        :return: The selected action  		  	   		 	 	 			  		 			     			  	 
-        :rtype: int  		  	   		 	 	 			  		 			     			  	 
-        """  		  	   		 	 	 			  		 			     			  	 
-        self.s = s  		  	   		 	 	 			  		 			     			  	 
-        action = rand.randint(0, self.num_actions - 1)  		  	   		 	 	 			  		 			     			  	 
-        if self.verbose:  		  	   		 	 	 			  		 			     			  	 
-            print(f"s = {s}, a = {action}")  		  	   		 	 	 			  		 			     			  	 
-        return action  		  	   		 	 	 			  		 			     			  	 
-  		  	   		 	 	 			  		 			     			  	 
-    def query(self, s_prime, r):  		  	   		 	 	 			  		 			     			  	 
-        """  		  	   		 	 	 			  		 			     			  	 
-        Update the Q table and return an action  		  	   		 	 	 			  		 			     			  	 
-  		  	   		 	 	 			  		 			     			  	 
-        :param s_prime: The new state  		  	   		 	 	 			  		 			     			  	 
-        :type s_prime: int  		  	   		 	 	 			  		 			     			  	 
-        :param r: The immediate reward  		  	   		 	 	 			  		 			     			  	 
-        :type r: float  		  	   		 	 	 			  		 			     			  	 
-        :return: The selected action  		  	   		 	 	 			  		 			     			  	 
-        :rtype: int  		  	   		 	 	 			  		 			     			  	 
-        """  		  	   		 	 	 			  		 			     			  	 
-        action = rand.randint(0, self.num_actions - 1)  		  	   		 	 	 			  		 			     			  	 
-        if self.verbose:  		  	   		 	 	 			  		 			     			  	 
-            print(f"s = {s_prime}, a = {action}, r={r}")  		  	   		 	 	 			  		 			     			  	 
-        return action  		  	   		 	 	 			  		 			     			  	 
-  		  	   		 	 	 			  		 			     			  	 
-  		  	   		 	 	 			  		 			     			  	 
-if __name__ == "__main__":  		  	   		 	 	 			  		 			     			  	 
-    print("Remember Q from Star Trek? Well, this isn't him")  		  	   		 	 	 			  		 			     			  	 
+class QLearner(object):
+    """
+    This is a Q learner object.
+
+    :param num_states: The number of states to consider.
+    :type num_states: int
+    :param num_actions: The number of actions available..
+    :type num_actions: int
+    :param alpha: The learning rate used in the update rule. Should range between 0.0 and 1.0 with 0.2 as a typical value.
+    :type alpha: float
+    :param gamma: The discount rate used in the update rule. Should range between 0.0 and 1.0 with 0.9 as a typical value.
+    :type gamma: float
+    :param rar: Random action rate: the probability of selecting a random action at each step. Should range between 0.0 (no random actions) to 1.0 (always random action) with 0.5 as a typical value.
+    :type rar: float
+    :param radr: Random action decay rate, after each update, rar = rar * radr. Ranges between 0.0 (immediate decay to 0) and 1.0 (no decay). Typically 0.99.
+    :type radr: float
+    :param dyna: The number of dyna updates for each regular update. When Dyna is used, 200 is a typical value.
+    :type dyna: int
+    :param verbose: If “verbose” is True, your code can print out information for debugging.
+    :type verbose: bool
+    """
+    def __init__(
+        self,
+        num_states=100,
+        num_actions=4,
+        alpha=0.2,
+        gamma=0.9,
+        rar=0.5,
+        radr=0.99,
+        dyna=0,
+        verbose=False,
+    ):
+        """
+        Constructor method
+        """
+        self.verbose = verbose
+        self.num_actions = num_actions
+        self.s = 0
+        self.a = 0
+        self.num_states = num_states
+        self.num_actions = num_actions
+        self.alpha = alpha
+        self.gamma = gamma
+        self.rar = rar
+        self.radr = radr
+        self.dyna = dyna
+
+        if dyna > 0:
+            self.experience_tuples = []  # List of (s, a, s', r) tuples
+
+
+        self.q = np.random.uniform(-1.0, 1.0, (num_states, num_actions))
+
+    def querysetstate(self, s):
+        """
+        Update the state without updating the Q-table
+
+        :param s: The new state
+        :type s: int
+        :return: The selected action
+        :rtype: int
+        """
+        self.s = s
+        action = rand.randint(0, self.num_actions - 1)
+
+        # if rand.random() > self.rar:
+        if np.random.random() > self.rar:
+            action = np.argmax(self.q[self.s, :]) # max q val in row s of q table
+
+        if self.verbose:
+            print(f"s = {s}, a = {action}")
+
+        return action
+
+
+
+    def query(self, s_prime, r):
+        """
+        Update the Q table and return an action
+
+        :param s_prime: The new state
+        :type s_prime: int
+        :param r: The immediate reward
+        :type r: float
+        :return: The selected action
+        :rtype: int
+        """
+        if self.dyna > 0:
+            self.experience_tuples.append((self.s, self.a, s_prime, r))
+
+        # Update Q table
+        self.q[self.s, self.a] = (1 - self.alpha) * self.q[self.s, self.a] + self.alpha * (r + self.gamma * np.max(self.q[s_prime, :]))
+
+        if self.dyna>0:
+            self.perform_dyna()
+
+        action = rand.randint(0, self.num_actions - 1)
+        # if rand.random() > self.rar:
+        if np.random.random() > self.rar:
+            action = np.argmax(self.q[s_prime, :]) # max q val in row s of q table
+
+        # update state
+        self.rar = self.rar * self.radr
+        self.s = s_prime
+        self.a = action
+
+        if self.verbose:
+            print(f"s = {s_prime}, a = {action}, r={r}")
+        return action
+
+
+
+    def perform_dyna(self):
+        """
+           Perform Dyna-Q planning - hallucinate experience and update Q
+           """
+        if len(self.experience_tuples) == 0:
+            return
+
+        # hallucinate from experience tuples directly in Q-table
+        for _ in range(self.dyna):
+            idx = np.random.randint(0, len(self.experience_tuples))
+            s, a, s_prime, r = self.experience_tuples[idx]
+
+            # Update Q-table with this hallucinated experience
+            self.q[s, a] = (1 - self.alpha) * self.q[s, a] + self.alpha * (r + self.gamma * np.max(self.q[s_prime, :]))
+
+    def author(self):
+        return 'lliao32'
+
+    def study_group(self):
+        return "lliao32"
+
+
+if __name__ == "__main__":
+    print("Remember Q from Star Trek? Well, this isn't him")
+
+
